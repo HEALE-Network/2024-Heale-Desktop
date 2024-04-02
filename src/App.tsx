b8e247dc053ca04cb6d99757ed0c7cc364c4136d
Syed Hamza Hussain <hamza@heateddetails.com>
2024-04-02 07:27:03 +0500

    latest work till 1st april

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import PersonalAccount from "modules/onboarding/personal/personal-account";
-import AuthLayout from "modules/shared/AuthLayout";
-import Welcome from "modules/welcome";
+import { ChakraProvider } from '@chakra-ui/react';
+import { ToastContainer } from 'react-toastify';
+import PersonalAccount from 'modules/onboarding/personal/personal-account';
+import AuthLayout from 'modules/shared/AuthLayout';
+import Welcome from 'modules/welcome';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import theme from 'theme';
+import 'App.css';
+import BusinessAccount from 'modules/onboarding/business/business-account';
+import Broker from 'modules/onboarding/business/broker';
+import Lender from 'modules/onboarding/business/lender';
+import Carrier from 'modules/onboarding/business/Carrier';
+import 'react-toastify/dist/ReactToastify.css';
 import BrowserExtension from "modules/browserExtension";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import theme from "theme";
 import "App.css"
-import BusinessAccount from "modules/onboarding/business/business-account";
+import DashboardLayout from "modules/shared/DashboardLayout";
+import Dashboard from "modules/dashboard";
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
+      <ToastContainer />
       <BrowserRouter>
         <Routes>
           <Route element={<AuthLayout />}>
@@ -18,6 +27,12 @@ function App() {
             <Route path="personal" element={<PersonalAccount />} />
             <Route path="extension" element={<BrowserExtension />} />
             <Route path="business" element={<BusinessAccount />} />
+            <Route path="business/broker" element={<Broker />} />
+            <Route path="business/lender" element={<Lender />} />
+            <Route path="business/carrier" element={<Carrier />} />
+          </Route>
+          <Route element={<DashboardLayout />}>
+            <Route path="dashboard" element={<Dashboard />} />
           </Route>
         </Routes>
       </BrowserRouter>
